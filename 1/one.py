ddcca2ee83838ecbe91875420884081c6c0de793
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2020-12-01 15:43:47 +0000

    Day one

---
+++ 1/one.py
@@ -0,0 +1,17 @@
+with open("input.txt") as file:
+    raw = file.read().split("\n")
+data = [int(item) for item in raw]
+
+indices = range(len(data))
+solved = False
+for i in indices:
+    a = data[i]
+    for j in filter(lambda x: x is not i, indices):
+        b = data[j]
+        if a + b == 2020:
+            print(f"{a} * {b} = {a * b}")
+            solved = True
+        if solved:
+            break
+    if solved:
+        break
---
+++ 1/two.py
@@ -0,0 +1,21 @@
+with open("input.txt") as file:
+    raw = file.read().split("\n")
+data = [int(item) for item in raw]
+
+solved = False
+indices = range(len(data))
+for i in indices:
+    a = data[i]
+    for j in filter(lambda x: x is not a, indices):
+        b = data[j]
+        for k in filter(lambda x: x not in [i, j], indices):
+            c = data[k]
+            if a + b + c == 2020:
+                print(f"{a} * {b} * {c} = {a * b * c}")
+                solved = True
+            if solved:
+                break
+        if solved:
+            break
+    if solved:
+        break
