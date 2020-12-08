4a329e4559f4de323ab271c56af1370acd7605d8
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2020-12-08 17:20:40 +0000

    Add day 8

---
+++ 08/python/solve.py
@@ -0,0 +1,58 @@
+import fileinput
+
+lines = [line.replace("\n", "").split() for line in fileinput.input()]
+
+
+# Part 1
+i = 0
+done_indices = []
+acc = 0
+
+while not i in done_indices:
+    cmd, n = lines[i]
+    num = int(n)
+    if cmd == "acc":
+        acc += num
+        i += 1
+    elif cmd == "nop":
+        i += 1
+    elif cmd == "jmp":
+        i += num
+    done_indices.append(i)
+
+print("part1", acc)
+
+
+# Part2
+
+idx_to_swap = [i for i, line in enumerate(lines) if line[0]
+               == "jmp" or line[0] == "nop"]
+
+for i_swap in idx_to_swap:
+    done_indices = []
+    acc = 0
+    i = 0
+    while not i in done_indices:
+        if i >= len(lines):
+            break
+        cmd, n = lines[i]
+        num = int(n)
+        if cmd == "acc":
+            acc += num
+            i += 1
+        elif cmd == "nop":
+            if i == i_swap:
+                i += num
+            else:
+                i += 1
+        elif cmd == "jmp":
+            if i == i_swap:
+                i += 1
+            else:
+                i += num
+        done_indices.append(i)
+
+    if i == len(lines):
+        break
+
+print("Part2", acc)
